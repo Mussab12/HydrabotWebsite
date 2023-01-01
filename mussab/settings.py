649097677185323506227805b4ec068cf04c6866
Mussab12 <shahidmussab7@gmail.com>
2023-01-02 02:07:35 +0500

    fixed static files issue

--- mussab/settings.py
+++ mussab/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = "mussab.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "./water_store/templates")],
+        "DIRS": [os.path.join(BASE_DIR, "water_store/templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -137,10 +137,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'mussab/static')
+    os.path.join(BASE_DIR,'./mussab/static')
 ]
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles') 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT=os.path.join(BASE_DIR,'static') 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
